0a2ce3ca0f7f2fabbec5e0ffc686be33e1189fb7
NEWSIX <60424171+NEWSIX@users.noreply.github.com>
2020-02-26 14:34:14 +0700

    L298N TEST

--- EIEI.ino
+++ EIEI.ino
@@ -1,3 +1,30 @@
-5555555555555
-__TIMESTAMP__
-jhighigjihjik
+//Motor Pins
+int EN_A = 11;      //Enable pin for first motor
+int IN1 = 9;       //control pin for first motor
+int IN2 = 8;       //control pin for first motor
+int IN3 = 7;        //control pin for second motor
+int IN4 = 6;        //control pin for second motor
+int EN_B = 10;      //Enable pin for second motor
+//Initializing variables to store data
+int motor_speed=255;
+int motor_speed1;
+void setup ( ) {
+  Serial.begin (9600); //Starting the serial communication at 9600 baud rate
+  //Initializing the motor pins as output
+  pinMode(EN_A, OUTPUT);
+  pinMode(IN1, OUTPUT);  
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);  
+  pinMode(IN4, OUTPUT);
+  pinMode(EN_B, OUTPUT);
+  //Initializng the joystick pins as input
+                   
+
+}
+void loop () {
+
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    analogWrite(EN_A, motor_speed);
+
+}
