7d7eb7b9bfd977b92f1d6e965d7fa472dc970957
NEWSIX <60424171+NEWSIX@users.noreply.github.com>
2020-03-04 19:07:32 +0700

    Update Buzzer_Servo.ino
    IF PRESS 0 OR 1 = [SERVO ANGLE = 90 OR 0 AND 2 TIMES BUZZER ]

--- Buzzer_Servo.ino
+++ Buzzer_Servo.ino
@@ -9,42 +9,44 @@ void setup()
   Serial.begin(9600);
   servo_test.attach(9);
   pinMode(Buzzer, OUTPUT);
-  
 } 
 void loop() 
 {
-  if (Serial.available() > 0) 
+  if (Serial.available() > 0)
     {
-     key = Serial.read();
+      key = Serial.read(); 
       if (key == '0')
-        {
-         for(angle = 0; angle < 180; angle += 1)    // command to move from 0 degrees to 180 degrees 
-          {                                  
-            servo_test.write(angle);                 //command to rotate the servo to the specified angle
-            delay(15);                       
-          }          
-            delay(1000);
-            for(angle = 180; angle>=1; angle-=5)     // command to move from 180 degrees to 0 degrees 
+        { 
+         for (int i = 0; i < 2; i++) 
+          {
+           digitalWrite(Buzzer,LOW);
+           delay(100);
+           digitalWrite(Buzzer,HIGH);
+           delay(50);
+          }  
+         for(angle = 90; angle>=1; angle--)  
           {                                
-            servo_test.write(angle);              //command to rotate the servo to the specified angle
-            delay(5);                       
+           servo_test.write(angle);
+           delay(5);                       
           } 
-            delay(1000);
+           delay(1000);
         }
-        
+    
+       else if (key == '1') 
+        {
+          for (int i = 0; i < 2; i++) 
+           {
+            digitalWrite(Buzzer,LOW);
+            delay(100);
+            digitalWrite(Buzzer,HIGH);
+            delay(50);
+           }
+          for(angle = 0; angle < 90; angle ++)   
+           {                                
+            servo_test.write(angle);
+            delay(5);                       
+           }
+          delay(1000);
+        }
      }
-   else if (key == '1')
-    {
-     for (int i = 0; i < 3; i++) 
-     {
-     digitalWrite(Buzzer,LOW);
-     delay(100);
-     digitalWrite(Buzzer,HIGH);
-     delay(50);
-     }
-    }
-   else if (key == '2') 
-    {
-     Serial.println("Hello All We love Arduino ");
-    }
 }
