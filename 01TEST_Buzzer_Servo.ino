27fdeaa6d07f4d06cde86e9aef55889e442c1b83
NEWSIX <60424171+NEWSIX@users.noreply.github.com>
2020-03-10 13:40:21 +0700

    Buzzer_LED_Ultrasonic
    1. Input (1,0) for control Servor
    2. Ultrasonic Detecting 
    3. Signal Buzzer and LED

--- 01TEST_Buzzer_Servo.ino
+++ 01TEST_Buzzer_Servo.ino
@@ -3,6 +3,7 @@ char key = '0' ;
 Servo servo_test;                 
 int angle = 0; 
 int Buzzer = 4;
+int LED = 3;
 
 const int trigPin = 9;
 const int echoPin = 10;
@@ -15,67 +16,87 @@ void setup()
   Serial.begin(9600);
   servo_test.attach(2);
   pinMode(Buzzer, OUTPUT);
-
+  pinMode(LED, OUTPUT);
   pinMode(trigPin, OUTPUT); 
   pinMode(echoPin, INPUT);
  
 } 
+
 void loop() 
 {
+ Ultrasonic();
+ Mode();
+ 
+}
 
+void Ultrasonic()
+{
  digitalWrite(trigPin, HIGH);
  delay(1);
  digitalWrite(trigPin, LOW);
  duration = pulseIn(echoPin, HIGH);
  distance = (duration/2) / 29.1;
- digitalWrite(Buzzer, HIGH);
-
+ Serial.println(distance);
  if (distance <= 10)
  { 
   for (int i = 0; i < 2; i++) 
    {
     digitalWrite(Buzzer,LOW);
+    digitalWrite(LED,HIGH);
     delay(50);
     digitalWrite(Buzzer,HIGH);
+    digitalWrite(LED,LOW);
     delay(50);
-   }
+   }  
  }
- 
+}
+
+void Mode()
+{
   if (Serial.available() > 0)
     {
       key = Serial.read(); 
       if (key == '0')
         { 
-         for (int i = 0; i < 2; i++) 
-          {
-           digitalWrite(Buzzer,LOW);
-           delay(100);
-           digitalWrite(Buzzer,HIGH);
-           delay(50);
-          }  
-         for(angle = 90; angle>=1; angle--)  
+         Servo_0();
+         BuzzLED();
+        }
+       else if (key == '1') 
+        {
+         Servo_1();
+         BuzzLED();
+        }
+     }
+}
+
+void BuzzLED()
+{ 
+  digitalWrite(Buzzer, HIGH);
+  for (int i = 0; i < 2; i++) 
+   {
+    digitalWrite(Buzzer,LOW);
+    digitalWrite(LED,HIGH);
+    delay(100);
+    digitalWrite(Buzzer,HIGH);
+    digitalWrite(LED,LOW);
+    delay(50);
+   }
+  delay(1000);
+}
+
+void Servo_0()
+{
+  for(angle = 90; angle>=1; angle--)  
           {                                
            servo_test.write(angle);
            delay(5);                       
           } 
-           delay(1000);
-        }
-    
-       else if (key == '1') 
-        {
-          for (int i = 0; i < 2; i++) 
-           {
-            digitalWrite(Buzzer,LOW);
-            delay(100);
-            digitalWrite(Buzzer,HIGH);
-            delay(50);
-           }
-          for(angle = 0; angle < 90; angle ++)   
-           {                                
-            servo_test.write(angle);
-            delay(5);                       
-           }
-          delay(1000);
-        }
-     }
+}
+void Servo_1()
+{
+  for(angle = 0; angle < 90; angle ++)   
+          {                                
+           servo_test.write(angle);
+           delay(5);                       
+          } 
 }
