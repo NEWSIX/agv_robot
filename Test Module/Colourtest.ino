a172151132bfd2f79b65566c41d98c16ed973b5c
Fasuto1st <nicha.tnct@gmail.com>
2020-03-11 13:37:25 +0700

    add

---
+++ Test Module/Colourtest.ino
@@ -0,0 +1,69 @@
+const int s0 = 2;  
+const int s1 = 3;  
+const int s2 = 4;  
+const int s3 = 5;  
+const int out = 8;   
+
+// Variables  
+int red = 0;  
+int green = 0;  
+int blue = 0;  
+    
+void setup()   
+{  
+  Serial.begin(9600); 
+  pinMode(s0, OUTPUT);  
+  pinMode(s1, OUTPUT);  
+  pinMode(s2, OUTPUT);  
+  pinMode(s3, OUTPUT);  
+  pinMode(out, INPUT);  
+  digitalWrite(s0, HIGH);  
+  digitalWrite(s1, HIGH);  
+}  
+    
+void loop() 
+{  
+  color(); 
+  Serial.print("R Intensity:");  
+  Serial.print(red, DEC);  
+  Serial.print(" G Intensity: ");  
+  Serial.print(green, DEC);  
+  Serial.print(" B Intensity : ");  
+  Serial.print(blue, DEC);  
+  //Serial.println();  
+
+  if (red < blue && red < green && red > 20)
+  {  
+   Serial.println(" - (Red Color)");    
+  }  
+
+  else if (blue < red && blue < green)   
+  {  
+   Serial.println(" - (Blue Color)");  
+  }  
+
+  else if (green < red && green < blue)  
+  {  
+   Serial.println(" - (Green Color)");  
+
+  }  
+  else{
+  Serial.println();  
+  }
+  delay(300);   
+
+ }  
+    
+void color()  
+{    
+  digitalWrite(s2, LOW);  
+  digitalWrite(s3, LOW);  
+  //count OUT, pRed, RED  
+  red = pulseIn(out, digitalRead(out) == LOW ? LOW : HIGH);  
+  digitalWrite(s3, HIGH);  
+  //count OUT, pBLUE, BLUE  
+  blue = pulseIn(out, digitalRead(out) == LOW ? HIGH : HIGH);  
+  digitalWrite(s2, HIGH);  
+  //count OUT, pGreen, GREEN  
+  green = pulseIn(out, digitalRead(out) == HIGH ? HIGH : HIGH);  
+}
