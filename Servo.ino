b3cd4b2aa79b1f3162bec8407e8e9dfb4c03c02c
Fasuto1st <60424518+Fasuto1st@users.noreply.github.com>
2020-03-25 13:14:27 +0700

    Update Servo.ino
      }

--- Servo.ino
+++ Servo.ino
@@ -7,14 +7,14 @@ myservo.attach(9);
 }
 void loop()
 {
-  if(y=0){
-    myservo.write(90); 
-   Serial.println("A1");
-    delay(1000); 
-  }
   if(y=1){
     myservo.write(0);
     Serial.println("A2");
     delay(1000);
+  }  
+  if(y=0){
+    myservo.write(90); 
+   Serial.println("A1");
+    delay(1000); 
   }
 }
