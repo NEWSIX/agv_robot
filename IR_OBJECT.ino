547b5071d69be4ab03814ba259133b32c5cf4089
NEWSIX <nitigon.phad@bumail.net>
2020-03-25 16:37:00 +0700

    TEST & setpin

--- IR_OBJECT.ino
+++ IR_OBJECT.ino
@@ -1,13 +1,14 @@
 int a;
+int Object_Sensor = 33;
 void setup() 
 {
 Serial.begin(9600);
-pinMode(INPUT,6);
+pinMode(INPUT,Object_Sensor);
 }
 
 void loop() 
 {
-  a=digitalRead(6);
+  a=digitalRead(Object_Sensor);
   Serial.println(a);
  delay(200);
 }
