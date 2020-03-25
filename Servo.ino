bc49e1c77f430d3fb866f633aff3fb89cf25ce35
Fasuto1st <60424518+Fasuto1st@users.noreply.github.com>
2020-03-25 13:06:38 +0700

    Add Servo

---
+++ Servo.ino
@@ -0,0 +1,20 @@
+#include <Servo.h>
+Servo myservo; 
+int y = 1;
+void setup()
+{
+myservo.attach(9);
+}
+void loop()
+{
+  if(y=0){
+    myservo.write(90); 
+   Serial.println("A1");
+    delay(1000); 
+  }
+  if(y=1){
+    myservo.write(0);
+    Serial.println("A2");
+    delay(1000);
+  }
+}
