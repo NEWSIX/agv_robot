9e41dcc1c61d8f543e6d808fce00273fd2ddacf1
Fasuto1st <nicha.tnct@gmail.com>
2020-04-22 18:28:48 +0700

    Upload

--- EIEI.ino
+++ EIEI.ino
@@ -1,31 +1,98 @@
-//Motor Pins
-int EN_A = 11;      //Enable pin for first motor
-int IN1 = 9;       //control pin for first motor
-int IN2 = 8;       //control pin for first motor
-int IN3 = 7;        //control pin for second motor
-int IN4 = 6;        //control pin for second motor
-int EN_B = 10;      //Enable pin for second motor
-//Initializing variables to store data
-int motor_speed=255;
-int motor_speed1;
-void setup ( ) {
-  Serial.begin (9600); //Starting the serial communication at 9600 baud rate
-  //Initializing the motor pins as output
-  pinMode(EN_A, OUTPUT);
-  pinMode(IN1, OUTPUT);  
-  pinMode(IN2, OUTPUT);
-  pinMode(IN3, OUTPUT);  
-  pinMode(IN4, OUTPUT);
-  pinMode(EN_B, OUTPUT);
-  //Initializng the joystick pins as input
-                   
+#include <Arduino_FreeRTOS.h>
+const int Buzzer = 31;
+const int trigPin = 36;
+const int echoPin = 38;
 
+// define two tasks for Blink & AnalogRead
+void TaskBlink( void *pvParameters );
+void TaskAnalogRead( void *pvParameters );
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB, on LEONARDO, MICRO, YUN, and other 32u4 based boards.
+  }
+
+  // Now set up two tasks to run independently.
+  xTaskCreate(
+    TaskBlink
+    ,  "Blink"   // A name just for humans
+    ,  128  // This stack size can be checked & adjusted by reading the Stack Highwater
+    ,  NULL
+    ,  2  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+    ,  NULL );
+
+  xTaskCreate(
+    TaskAnalogRead
+    ,  "AnalogRead"
+    ,  128  // Stack size
+    ,  NULL
+    ,  1  // Priority
+    ,  NULL );
+
+  // Now the task scheduler, which takes over control of scheduling individual tasks, is automatically started.
 }
-void loop () {
 
-    digitalWrite(IN1, LOW);
-    digitalWrite(IN2, HIGH);
-    analogWrite(EN_A, motor_speed);
-
+void loop()
+{
+  // Empty. Things are done in Tasks.
 }
-testtttttttttttttt
+
+/*--------------------------------------------------*/
+/*---------------------- Tasks ---------------------*/
+/*--------------------------------------------------*/
+
+void TaskBlink(void *pvParameters)  // This is a task.
+{
+  (void) pvParameters;
+  pinMode(Buzzer, OUTPUT);
+  pinMode(trigPin, OUTPUT); 
+  pinMode(echoPin, INPUT);
+   for (;;)
+  {
+    Ultrasonic(); 
+      }
+}
+/*----------------------------------------------------Ultrasonic-------------------------------------*/
+void Ultrasonic() {
+  digitalWrite(trigPin,HIGH);
+  delay(5);
+  digitalWrite(trigPin,LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;
+  Serial.println(distance);
+    if (distance <= 10){
+      BuzzLED();
+    }
+    else{
+    }
+}
+/*----------------------------------------------------BUZZER-------------------------------------*/
+void BuzzLED(){ 
+  digitalWrite(Buzzer, HIGH);
+  for (int i = 0; i < newsix; i++) {
+    digitalWrite(Buzzer,LOW);
+    delay(100);
+    digitalWrite(Buzzer,HIGH);
+    delay(50);
+  }
+}  
+
+void TaskAnalogRead(void *pvParameters)  // This is a task.
+{
+  (void) pvParameters;
+
+
+  for (;;)
+  {
+    // read the input on analog pin 0:
+    int sensorValue = analogRead(A0);
+    // print out the value you read:
+    Serial.println(sensorValue);
+    vTaskDelay(1);  // one tick delay (15ms) in between reads for stability
+  }
+}
