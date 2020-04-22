a29a8ac573b47c0b4b07327363850d531507f31c
Fasuto1st <nicha.tnct@gmail.com>
2020-04-22 18:18:10 +0700

    Ultrasonic_BuzzerTest

--- Test Module/Ultrasonic_test.ino
+++ Test Module/Ultrasonic_test.ino
@@ -1,38 +1,39 @@
-const int pingPin = 13;
-    int inPin = 12;
+int Buzzer = 31;
+const int trigPin = 36;
+int echoPin = 38;
+long duration;
+int distance;
 
-     
-    void setup() {
-      Serial.begin(9600);
+void setup() {
+  Serial.begin(9600);
+  pinMode(trigPin,OUTPUT);
+  pinMode(echoPin,INPUT);
+  pinMode(Buzzer, OUTPUT);
+}
+
+void loop() {
+  Ultrasonic();
+}
+
+void Ultrasonic() {
+  digitalWrite(trigPin,HIGH);
+  delay(5);
+  digitalWrite(trigPin,LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;
+  Serial.println(distance);
+    if (distance <= 10){
+      BuzzLED();
     }
-     
-    void loop()
-    {
-      long duration, cm;
-     
-      pinMode(pingPin, OUTPUT);
-     
-
-      digitalWrite(pingPin, LOW);
-      delayMicroseconds(2);
-      digitalWrite(pingPin, HIGH);
-      delayMicroseconds(5);
-      digitalWrite(pingPin, LOW);
-      pinMode(inPin, INPUT);
-      duration = pulseIn(inPin, HIGH);
-
-      cm = microsecondsToCentimeters(duration);
-
-      Serial.print(cm);
-      Serial.print("cm");
-      Serial.println();     
-      delay(100);
+    else{
     }
-     
-    long microsecondsToCentimeters(long microseconds)
-    {
-      // The speed of sound is 340 m/s or 29 microseconds per centimeter.
-      // The ping travels out and back, so to find the distance of the
-      // object we take half of the distance travelled.
-      return microseconds / 29 / 2;
-    }
+}
+void BuzzLED(){ 
+  digitalWrite(Buzzer, HIGH);
+  for (int i = 0; i < newsix; i++) {
+    digitalWrite(Buzzer,LOW);
+    delay(100);
+    digitalWrite(Buzzer,HIGH);
+    delay(50);
+   }
+}
